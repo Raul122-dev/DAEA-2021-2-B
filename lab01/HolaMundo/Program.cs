73724b642eddb2ebc5394b032ff9fc1a229f3698
Raul122 <victor.huaycho@tecsup.edu.pe>
2021-08-23 12:20:04 -0500

    lab01 parte 1

---
+++ lab01/HolaMundo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Como te llamas : ");
+            String nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre);
+        }
+    }
+}
