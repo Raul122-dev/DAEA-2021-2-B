340d99918a92fa69f2b37207246d6f2e6b2a1f4e
Raul122 <victor.huaycho@tecsup.edu.pe>
2021-08-27 13:41:12 -0500

    lab01 parte 1 (Funciones resta, multiplicacion y division)

---
+++ lab01/prjLAB01-2/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace prjLAB01_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dni = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.codigo,
+            this.dni});
+            this.dataGridView1.Location = new System.Drawing.Point(155, 255);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // codigo
+            // 
+            this.codigo.HeaderText = "Codigo";
+            this.codigo.Name = "codigo";
+            // 
+            // dni
+            // 
+            this.dni.HeaderText = "DNI";
+            this.dni.Name = "dni";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dni;
+    }
+}
+
---
+++ lab01/prjLab01-1/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjLab01_1
+{
+    class Program
+    {
+        //Funcion para calcular la suma de 2 numeros enteros
+        static int Suma(int a, int b)
+        {
+            return a + b;
+        }
+        //Funcion para calcular la resta de 2 numeros
+        static int Resta(int a, int b)
+        {
+            return a - b;
+        }
+        //Funcion para calcular la multiplicacion de 2 numeros
+        static int Multiplicacion(int a, int b)
+        {
+            return a * b;
+        }
+        //Funcion para calcular la division de 2 numeros
+        static Decimal Division(int a, int b)
+        {
+            return Convert.ToDecimal(a) / Convert.ToDecimal(b);
+        }
+        //Procedimiento que imprime la raiz cuadrada de los 10 primero numeros
+        static void Raiz()
+        {
+            for (int i = 1; i <= 10; i++) {
+                Console.WriteLine("La raiz cuadrada de {0} es : {1}", i, Math.Sqrt(i));
+            }
+        }
+        static void Main(string[] args)
+        {
+            //variables para los numeros
+            int a;
+            int b;
+
+            Console.Title = "Procedimientos y Funciones";
+            string opcion;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("[1] Suma de dos numeros");
+                Console.WriteLine("[2] Resta de dos numeros");
+                Console.WriteLine("[3] Multiplicacion de dos numeros");
+                Console.WriteLine("[4] Division de dos numeros");
+                Console.WriteLine("[5] Imprimir la raiz cuadrada de los 10 primeros numeros enteros");
+                Console.WriteLine("[0] Salir");
+                Console.WriteLine("Ingrese una opcion y presion ENTER");
+                opcion = Console.ReadLine();
+
+
+                switch (opcion)
+                {
+                    case "1":
+                        Console.WriteLine("Ingrese el primer numero");
+                        a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Ingrese el segundo numero");
+                        b = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("La suma de {0} y {1} es {2}", a, b, Suma(a, b));
+                        Console.ReadKey();
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Ingrese el primer numero");
+                        a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Ingrese el segundo numero");
+                        b = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("La Resta de {0} y {1} es {2}", a, b, Resta(a, b));
+                        Console.ReadKey();
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Ingrese el primer numero");
+                        a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Ingrese el segundo numero");
+                        b = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("La Multiplicacion de {0} y {1} es {2}", a, b, Multiplicacion(a, b));
+                        Console.ReadKey();
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Ingrese el primer numero");
+                        a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Ingrese el segundo numero");
+                        b = Convert.ToInt32(Console.ReadLine());
+
+                        if (b == 0)
+                        {
+                            Console.WriteLine("No se puede divir entre 0");
+                        }
+                        else { 
+                            Console.WriteLine("La Division de {0} y {1} es {2}", a, b, Division(a, b));
+                        }
+                        
+                        Console.ReadKey();
+                        break;
+
+                    case "5":
+                        Console.WriteLine("Calculando...");
+                        Raiz();
+                        Console.ReadKey();
+                        break;
+                }
+            } while (!opcion.Equals("0"));
+            
+        }
+    }
+}
