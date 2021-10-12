f4ea939ec09eb3486a62a636dc312e03db0fbdd3
Raul122 <victor.huaycho@tecsup.edu.pe>
2021-10-11 19:31:29 -0500

    lab06 - Creacion del Diseño del formulario

--- Lab06/Form1.cs
+++ Lab06/Form1.cs
@@ -7,14 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Lab06
 {
     public partial class Form1 : Form
     {
+
+        SqlConnection con;
+        DataSet ds = new DataSet();
+        DataTable tablePerson = new DataTable();
+
         public Form1()
         {
             InitializeComponent();
         }
+
     }
 }
