b7515bfb53411a393c86dbec1a169d04db0e8722
raul rebel game <raul.rebelgame@gmail.com>
2021-11-09 18:05:12 -0500

    Lab12 - Realizacion del proyecto ASP MVC

---
+++ Lab12/Controllers/PersonaController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab12.Models;
+
+namespace Lab12.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Listar()
+        {
+            List<Persona> personas = new List<Persona>();
+            personas.Add(new Persona
+            {
+                PersonaID = 1,
+                Nombre = "Juan",
+                Apellido = "Perez",
+                Direccion = "Av. Evergreen 123",
+                FechaNac = Convert.ToDateTime("1997-11-07"),
+                Email = "juan@mail.com"
+            });
+            personas.Add(new Persona
+            {
+                PersonaID = 2,
+                Nombre = "Maria",
+                Apellido = "Salas",
+                Direccion = "Av. Progreso 325",
+                FechaNac = Convert.ToDateTime("1995-10-28"),
+                Email = "maria@mail.com"
+            });
+            personas.Add(new Persona
+            {
+                PersonaID = 3,
+                Nombre = "Carlos",
+                Apellido = "Martinez",
+                Direccion = "Av. Los manzanos 101",
+                FechaNac = Convert.ToDateTime("1982-02-14"),
+                Email = "carlos@mail.com"
+            });
+            return View(personas);
+        }
+
+    }
+}
