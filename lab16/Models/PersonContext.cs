409cd4e1e8bfeb59f63bc5100beab026ccf47396
RaulDev <Victor.huaycho@tecsup.edu.pe>
2021-12-07 15:59:06 -0500

    Lab16 - Creacion de una Api

---
+++ lab16/Models/PersonContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace persons.Models
+{
+    public class PersonContext: DbContext
+    {
+        public PersonContext(DbContextOptions<PersonContext> options)
+            : base(options)
+            {}
+            public DbSet<Person> Person {get; set;}
+    }
+}
